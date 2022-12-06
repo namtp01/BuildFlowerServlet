835fb05710a05c45fff8a970bbbcd44fbf5513c1
namtp01 <94774394+namtp01@users.noreply.github.com>
2022-12-06 14:42:06 +0700

    Update ProductModel.java

--- src/main/java/com/demo/models/ProductModel.java
+++ src/main/java/com/demo/models/ProductModel.java
@@ -10,5 +10,26 @@ public class ProductModel extends AbstractModel<ProductEntity>{
     public ProductModel() {
         super(ProductEntity.class);
     }
-
+    
+    @SuppressWarnings("unchecked")
+    public List<ProductEntity> findAll() {
+        List<ProductEntity> productEntities = null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            productEntities = session.createQuery("select * from ProductEntity ").list();
+//            org.hibernate.query.Query query = session.createQuery("from ProductEntity");
+//            query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            productEntities = null;
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return productEntities;
+    }
 }
